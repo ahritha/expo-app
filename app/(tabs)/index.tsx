7451fd7e5792caad2c4dc298886032a3d66cd0c1
Khon Angkearith <ahritha@gmail.com>
2025-01-07 22:10:33 +0700

    done

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,105 @@
 import { View, StyleSheet } from 'react-native';
-import { Image } from 'expo-image';
 import ImageViewer from '../components/ImageViewer';
+import Button from '../components/Button';
+import * as ImagePicker from 'expo-image-picker';
+import { useState , useRef } from 'react';
+import IconButton from '../components/IconButton';
+import CircleButton from '../components/CircleButton';
+import EmojiList from '../components/EmojiList';
+import EmojiPicker from '../components/EmojiPicker';
+import { ImageSource } from 'expo-image';
+import EmojiSticker from '../components/EmojiSticker';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import * as MediaLibrary from 'expo-media-library';
+import { captureRef } from 'react-native-view-shot';
 
-
-const PlaceholderImage = require('@/assets/images/background-image.png');
-
+const PlaceholderImage = require('@/assets/images/placeholder.png');
 
 export default function Index() {
+  const [image, setImage] = useState<string>()
+  const [showModal, setShowModal] = useState<boolean>(false)
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [pickedEmoji, setPickedEmoji] = useState<ImageSource | undefined>(undefined);
+  const [status, requestPermission] = MediaLibrary.usePermissions();
+  const imageRef = useRef<View>(null);
+
+  if (status === null) {
+    requestPermission();
+  }
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
+  const onReset = () => {
+    setShowModal(false)
+  }
+
+  const onUseImage = () => {
+    if (!image) return
+    setShowModal(true)
+  }
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+  };
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
+
+  const onSaveImageAsync = async () => {
+    try {
+      const localUri = await captureRef(imageRef, {
+        height: 440,
+        quality: 1,
+      });
+
+      await MediaLibrary.saveToLibraryAsync(localUri);
+      if (localUri) {
+        alert('Saved!');
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+
   return (
-    <View style={styles.container}>
+
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage}/>
+      <View style={styles.imageWrapper} ref={imageRef} collapsable={false}>
+          <ImageViewer imgSource={PlaceholderImage} selectedImage={image} />
+          {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
+        </View>
+        {
+          showModal ? <View style={{ flex: 1, justifyContent: 'center' }}>
+            <View style={{ flex: 1, justifyContent: 'center', flexDirection: 'row', alignItems: 'center' }} >
+              <IconButton icon="refresh" label="Reset" onPress={onReset} />
+              <CircleButton onPress={onAddSticker} />
+              <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+            </View>
+          </View> : <View>
+            <Button label="Choose a photo" onPress={pickImageAsync} theme="primary" />
+            <Button label="Use this photo" onPress={onUseImage} />
+          </View>
+        }
+        <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+          <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+        </EmojiPicker>
       </View>
-    </View>
+    </GestureHandlerRootView>
+
   );
 }
 
@@ -25,9 +112,15 @@ const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
   },
-  image: {
+  flexItem: {
+    flex: 1,
+  },
+
+  imageWrapper: {
+    position: 'relative',
     width: 320,
     height: 440,
     borderRadius: 18,
+    overflow: 'hidden',
   },
 });
---
+++ app/components/Button.tsx
@@ -0,0 +1,31 @@
+import { View, Text, Pressable } from 'react-native'
+import React from 'react'
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
+type ButtonProps = {
+    label: string
+    theme?: string
+    onPress?: () => void
+}
+const Button = ({ label, theme , onPress }: ButtonProps) => {
+
+    if (theme === 'primary') {
+        return (
+            <View  style={{ backgroundColor: 'white' , borderWidth : 4 , borderRadius: 10 , borderColor: "yellow" , padding: 10 , justifyContent: 'center' , alignItems: 'center' , height: 60 , marginVertical: 30  }} >
+                <Pressable style={{width: '100%', height: '100%', justifyContent: 'center' , alignItems: 'center',display: 'flex', flexDirection: 'row'}} onPress={onPress}>
+                    <FontAwesome style={{marginRight: 10}} name="picture-o" size={24} color="black" />	
+                    <Text >{label}</Text>
+                </Pressable>
+            </View>
+        )
+    }
+    return (
+        <View  style={{display:'flex' , justifyContent: "center" , alignItems:"center"}}>
+            <Pressable onPress={onPress}>
+                <Text style={{color: 'white'}} >{label}</Text>
+            </Pressable>
+        </View>
+    )
+}
+
+export default Button
